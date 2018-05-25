0e4d975377f945af00e10616c53ee359f0765092
snippet-java <snippet.java@gmail.com>
2018-05-25 15:40:43 -0400

    1.2x

--- Python For Newbies/Section 01/Module 02/combineage.py
+++ Python For Newbies/Section 01/Module 02/combineage.py
@@ -1,11 +1,28 @@
-<xml
-    xmlns="http://www.w3.org/1999/xhtml">
-    <variables></variables>
-    <block type="text_print" x="134" y="97">
-        <value name="TEXT">
-            <block type="text">
-                <field name="TEXT">your name</field>
-            </block>
+<xml xmlns="http://www.w3.org/1999/xhtml">
+  <variables></variables>
+  <block type="text_print" id=":wKeeLKYJhr-6J)]l@|Y" x="63" y="63">
+    <value name="TEXT">
+      <shadow type="text" id="%le||-.sGS#J:Iy0F6|C">
+        <field name="TEXT">abc</field>
+      </shadow>
+      <block type="text_join" id="l-#aBDm4$s6%2uGtIi$_">
+        <mutation items="3"></mutation>
+        <value name="ADD0">
+          <block type="text" id="2]x]Ug@sa$l+I0ZT|(vQ">
+            <field name="TEXT">name</field>
+          </block>
         </value>
-    </block>
+        <value name="ADD1">
+          <block type="text" id="7rbMlp8Dxwjvd.#;Q4D5">
+            <field name="TEXT"> </field>
+          </block>
+        </value>
+        <value name="ADD2">
+          <block type="text" id="E9g~uE/6n~yo(Uwdkkyf">
+            <field name="TEXT">age</field>
+          </block>
+        </value>
+      </block>
+    </value>
+  </block>
 </xml>
