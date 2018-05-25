dc8798bbc035989f165bef46766dc478d5ba24b4
Lakshminarayan Srinivasan <laksri@us.ibm.com>
2018-05-25 08:39:52 -0400

    init commit

---
+++ Python For Newbies/Section 01/Module 02/combineage.py
@@ -0,0 +1,11 @@
+<xml
+    xmlns="http://www.w3.org/1999/xhtml">
+    <variables></variables>
+    <block type="text_print" x="134" y="97">
+        <value name="TEXT">
+            <block type="text">
+                <field name="TEXT">your name</field>
+            </block>
+        </value>
+    </block>
+</xml>
